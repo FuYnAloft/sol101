1b8f511a0b12624db97ad29f0e7f47f9d333358d
FuYnAloft <207443530+fuynaloft@users.noreply.github.com>
2026-03-04 14:29:00 +0800

    ci: 添加自动更新工作流允许强制进行全量更新

--- update.py
+++ update.py
@@ -1,12 +1,17 @@
 import os
+import sys
 
 import requests
 
 from config import ANSWERS
 
+# 如果命令行参数中包含 --force，则强制全量更新，否则使用 ETag 进行增量更新
+FORCE_UPDATE = '--force' in sys.argv
+
+
 def sync_github(url, etag_path):
     headers = {}
-    if os.path.exists(etag_path):
+    if not FORCE_UPDATE and os.path.exists(etag_path): # 如果强制全量更新，则不使用 ETag 进行增量更新
         with open(etag_path, "r") as f:
             etag = f.read().strip()
             if etag:
@@ -50,9 +55,6 @@ def main():
         print('true')
     else:
         print('false')
-
-
-
 
 
 if __name__ == '__main__':
