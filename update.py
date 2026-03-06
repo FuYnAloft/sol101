c188a5af70981c6157b181ae9d38fb22691b39d3
FuYnAloft <207443530+fuynaloft@users.noreply.github.com>
2026-03-06 17:32:14 +0800

    refactor: 重构了 update.py ，现在更新时总是会下载所有文件

--- update.py
+++ update.py
@@ -1,62 +1,73 @@
-import os
-import sys
+import os.path
+from dataclasses import dataclass
+from typing import Iterable
 
 import requests
 
-from config import ANSWERS
-
-# 如果命令行参数中包含 --force，则强制全量更新，否则使用 ETag 进行增量更新
-FORCE_UPDATE = '--force' in sys.argv
+from config import Answer, ANSWERS
 
 
-def sync_github(url, etag_path):
-    headers = {}
-    if not FORCE_UPDATE and os.path.exists(etag_path): # 如果强制全量更新，则不使用 ETag 进行增量更新
+@dataclass(frozen=True)
+class UpdateResult:
+    updated: bool
+    content: str | None = None
+    etag: str | None = None
+
+
+def update_github(url: str, etag: str | None) -> UpdateResult:
+    headers: dict[str, str] = {}
+    if etag:
+        headers["If-None-Match"] = etag
+
+    response = requests.get(url, headers=headers, timeout=10)
+
+    match response.status_code:
+        case 304:
+            return UpdateResult(False)
+
+        case 200:
+            etag = response.headers.get("ETag")
+            content = response.text
+            return UpdateResult(True, content, etag)
+
+        case _:
+            raise Exception(f"Unexpected status code: {response.status_code}")
+
+
+def update_answer(answer: Answer) -> bool:
+    etag_path = f"etag/{answer.name}.etag"
+    if os.path.exists(etag_path):
         with open(etag_path, "r") as f:
             etag = f.read().strip()
-            if etag:
-                headers["If-None-Match"] = etag
-
-    resp = requests.get(url, headers=headers, timeout=10)
-
-    if resp.status_code == 304:
-        # 文件未变化
-        return -1, ""
-
-    elif resp.status_code == 200:
-        # 文件更新
-        etag = resp.headers.get("ETag")
-        if etag:
-            os.makedirs(os.path.split(etag_path)[0], exist_ok=True)
-            with open(etag_path, "w") as f:
-                f.write(etag)
-
-        content = resp.text
-        return 0, content
-
     else:
-        # 其他错误
-        raise requests.HTTPError(resp.status_code)
+        etag = None
+    result = update_github(answer.url, etag)
+    updated = result.updated
+
+    # 如果没更新，把 etag 设成 None 强制下载，并覆盖 result
+    if not updated:
+        result = update_github(answer.url, None)
+
+    os.makedirs('original', exist_ok=True)
+    with open(f'original/{answer.name}.md', 'w', encoding='utf-8') as f:
+        f.write(result.content or "")
+    os.makedirs('etag', exist_ok=True)
+    with open(etag_path, "w") as f:
+        f.write(result.etag or "")
+
+    return updated
 
 
-def main():
-    changed = 0
-    for answer in ANSWERS:
-        name = answer.name
-        url = answer.url
-        ret, content = sync_github(url, rf"etag/{name}")
-        if ret == 0:
-            print(f"{name} updated")
-            changed += 1
-            os.makedirs('original', exist_ok=True)
-            with open(f'original/{name}.md', 'w', encoding='utf-8') as f:
-                f.write(content)
-    print(f"{changed} files updated")
-    if changed:
-        print('true')
-    else:
-        print('false')
+def update(answers: Iterable[Answer]) -> bool:
+    updated = False
+    for answer in answers:
+        if update_answer(answer):
+            print(f"Updated {answer.name}")
+            updated = True
+        else:
+            print(f"No update for {answer.name}")
+    return updated
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    update(ANSWERS)
